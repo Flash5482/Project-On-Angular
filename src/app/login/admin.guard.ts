02d850255f84736184d5b0a729620b4e6bc0d874
Tretyak Andru <andruuu5482@gmail.com>
2021-08-18 11:12:00 +0300

    add guard

---
+++ src/app/login/admin.guard.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {PizzaService} from "../pizza-service.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  isAdmin: boolean | any;
+
+  constructor(private route: Router, service: PizzaService) {
+
+    this.isAdmin = localStorage.getItem("isAdmin");
+    this.isAdmin = JSON.parse(this.isAdmin);
+
+    if (!this.isAdmin) {
+      this.route.navigate(['pizza']);
+    }
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log(this.isAdmin);
+    return this.isAdmin;
+  }
+}
